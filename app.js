77edb4ccea87a71cdc07472aea433a170d2546dc
Nasser Boukehil <boukehil.nasreddine@gamil.com>
2020-04-18 23:23:01 +0200

    added colors generation feature with chromaJs

---
+++ app.js
@@ -0,0 +1,43 @@
+// Global Selections and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('generate');
+const sliders = document.querySelectorAll('input[type="range"]'); // selecting certain type of inputs
+const currentHexes = document.querySelectorAll('.color h2'); // select the h2 inside div of class color
+let initialColors;
+
+
+// Functions
+
+/*
+generate a color without chromaJs
+
+function generateHex() {
+    const letters = '#0123456789ABCDEF';
+    let hash = '#';
+    for (let i = 0; i < 6; i++) {
+        hash += letters[Math.floor(Math.random() * 16)];
+    }
+    return hash;
+}
+*/
+
+// generate a hexCode using chroma
+function generateHex() {
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+
+/** generates random colors at doc ready */
+function randomColors() {
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0]; // get the h2 of the div
+        const randomColor = generateHex();
+        // add the color to the div background
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+
+    });
+}
+
+randomColors();
